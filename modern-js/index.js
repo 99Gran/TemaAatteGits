a3475b952a2d44ca69911ac393fda5741a9edbda
99Gran <nini.g94@gmail.com>
2020-04-24 14:11:34 +0200

    filter commit

--- modern-js/index.js
+++ modern-js/index.js
@@ -1,4 +1,4 @@
-// OPPGAVE 1
+/////////////////////////////// OPPGAVE 1
 
 // Gammel kode
 /*
@@ -12,11 +12,10 @@ function fortellEnVits() {
 const fortellEnVits = () => {}
 console.log("Ka sa den ene tørrfesken te den andre?");
 console.log("Long time no sea");
+// NINI, YOU MADE IT
 
 
-
-
-// OPPGAVE 2
+/////////////////////////////// OPPGAVE 2
 
 // Gammel kode
 /*
@@ -32,11 +31,62 @@ console.log(addMeToTheSong('Tom'))
 // NINI, YOU MADE IT
 
 
-
-// OPPGAVE 3
+/////////////////////////////// OPPGAVE 3
 
 const tallene = [12, 82, 45, 56, 67, 4, 34, 32, 95, 89]
 
 /*const underAatti = (tallene) => {
     age >= 20;
 }*/
+
+
+/////////////////////////////// OPPGAVE 4
+
+const musikere = [
+  {navn: "Annifrid", rating: 6},
+  {navn: "Agnethe", rating: 3},
+  {navn: "Benny", rating: 5},
+  {navn: "Bjørn", rating: 2}
+]
+
+const merEnnFire = musikere.filter(element => element.rating > 4);
+
+console.log(merEnnFire)
+// NINI, YOU MADE IT
+
+
+/////////////////////////////// OPPGAVE 5
+
+const skurker = [
+  {navn: "Billy the Kid", egenskap: "Rask på avtrekkeren"},
+  {navn: "Jesse James", egenskap: "Iskald"},
+  {navn: "Brødrene Dalton", egenskap: "Jobber godt sammen"}
+]
+
+const godPaa = skurker.filter(element => element.egenskap += 'Jessie James')
+
+console.log(godPaa)
+
+
+/////////////////////////////// OPPGAVE 6
+
+//const ulMatretter = document.querySelector("#ulMatretter")
+
+//const matretter = ["Kapteinens favoritt", "Kjøtt utklemt i panne", "Biff Stroganof"]
+
+// Her kommer din kode
+
+
+/////////////////////////////// OPPGAVE 7
+
+//const words1 = ["The", "way", "you"]
+//const words2 = ["make", "me", "feel"]
+
+//const theTitle = // Her legger du inn koden som setter sammen arrayene i ett array
+//console.log(theTitle)
+
+
+/////////////////////////////// OPPGAVE 8
+
+/*let sitat = ["har", "gull", "i"]
+sitat = // Her kommer din kode*/
